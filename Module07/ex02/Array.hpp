8cf0d68bce905ee8a30c526a2e733d9325b01158
Abdelhamid Bouazi <abdelhamidbouazi@Abdelhamids-MacBook-Pro.local>
2023-10-09 02:52:00 +0100

    cahnges

--- Module07/ex02/Array.hpp
+++ Module07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: abouazi <abouazi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 17:20:21 by abouazi           #+#    #+#             */
-/*   Updated: 2023/10/02 17:39:29 by abouazi          ###   ########.fr       */
+/*   Updated: 2023/10/09 02:42:25 by abouazi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,57 @@
 
 #include <iostream>
 
+template <typename T>
 class Array
 {
-	private:
-	public:
-		Array();
-		~Array();
+    private:
+		int _size;
+		T *arr;
+    public:
+        Array() : _size(0){
+			arr =  new T();
+		}
+
+        Array(unsigned int n) : _size(n)
+        {
+            arr =  new T(n);
+        }
+
+		Array(const Array &cp)
+        {
+            _size = cp._size;
+            arr = new T[_size];
+            for(int i = 0; i < cp._size; i++)
+                arr[i] = cp.arr[i];
+        }
+		Array<T>& operator=(const Array& other)
+		{
+			if (this != &other)
+			{
+				if (arr)
+					delete[] arr;
+				_size = other._size;
+				arr = new T[_size];
+				for (int i = 0; i < _size; ++i)
+					arr[i] = other.arr[i];
+			}
+			return *this;
+		}
+
+		int size() {
+			return _size;
+		}
+        T &operator[](int i)
+        {
+            if(i < _size && i >= 0)
+                return arr[i];
+            throw std::out_of_range("Index out of bounds: Accessing elements beyond the valid range!");
+        }
+        ~Array()
+        {
+            delete[] arr;
+        }
 };
-
-Array::Array()
-{
-}
-
-Array::~Array()
-{
-}
 
 
 # endif
--- Module07/ex02/main.cpp
+++ Module07/ex02/main.cpp
@@ -6,7 +6,35 @@
 /*   By: abouazi <abouazi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 17:20:26 by abouazi           #+#    #+#             */
-/*   Updated: 2023/10/02 17:20:27 by abouazi          ###   ########.fr       */
+/*   Updated: 2023/10/09 02:50:31 by abouazi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+int main()
+{
+	Array<char> a(23);
+	Array<char> b;
+	Array<char> c(a);
+	try {
+		Array<char> d(4);
+
+		d[0] = 'O';
+		d[1] = 'P';
+		d[2] = 'U';
+		d[3] = 'S';
+
+
+		std::cout << "testing array : " << d[0] << std::endl;
+		std::cout << "testing array : " << d[1] << std::endl;
+		std::cout << "testing array : " << d[2] << std::endl;
+		std::cout << "testing array : " << d[3] << std::endl;
+		d[4] = '-';
+		std::cout << "testing array : " << d[4] << std::endl;
+	}
+	catch (const std::out_of_range& e){
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
+	return 0;
+}
