6b2dc623461f173e2ccb74fac7999937b687077a
Abdelhamid Bouazi <abdelhamidbouazi@Abdelhamids-MacBook-Pro.local>
2023-10-10 18:04:49 +0100

    ex00

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easyFind.hpp                                       :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abouazi <abouazi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:01:23 by abouazi           #+#    #+#             */
-/*   Updated: 2023/10/10 17:00:33 by abouazi          ###   ########.fr       */
+/*   Updated: 2023/10/10 17:57:18 by abouazi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 template <typename T>
-void	easyfind(T first, int second) {
-	int i;
+int	easyfind(T& first, int second) {
+    typename T::iterator it = std::find(first.begin(), first.end(), second);
 
-	// std::vector<int>::iterator it = first
-	i = 0;
-	find(it.begin(), (it.end() - 1), second);
-	// while (1) {
-	// 	i++;
-	// }
-	std::cout << "the first occurence is: " << i <<" which is " <<  first.at(i) << std::endl;;
+    if (it == first.end())
+		throw (std::out_of_range("Not Found : Iterator got out of range !"));
+    return std::distance(first.begin(), it);
 }
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -6,13 +6,28 @@
 /*   By: abouazi <abouazi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:01:27 by abouazi           #+#    #+#             */
-/*   Updated: 2023/10/10 16:05:44 by abouazi          ###   ########.fr       */
+/*   Updated: 2023/10/10 18:03:04 by abouazi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
 int main(){
-	easyfind("Abdelhamid", 22);
+	std::vector<int> myContainer;
+
+	myContainer.push_back(10);
+	myContainer.push_back(20);
+	myContainer.push_back(30);
+	myContainer.push_back(40);
+	myContainer.push_back(50);
+
+	try {
+		std::cout << easyfind(myContainer, 20) << std::endl;
+		std::cout << easyfind(myContainer, 1) << std::endl;
+		std::cout << easyfind(myContainer, 5) << std::endl;
+	}
+	catch (std::exception& e){
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
